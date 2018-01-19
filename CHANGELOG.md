f1890ba1550e1b764c2d7b476e3ce5cf3d9a873b
Shogun <shogun@cowtech.it>
2018-01-19 12:53:47 +0100

    Updated CHANGELOG.md.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 2018-01-19 / 0.3.1
+
+* Rename confusing methods.
+
 ### 2018-01-19 / 0.3.0
 
 * Only ship JS version with TS declarations.
--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,4 +1,4 @@
-export function load<T>(key: string, def: T = null): T{
+export function loadFromLocalStorage<T>(key: string, def: T = null): T{
   try{
     const serialized: string = localStorage.getItem(key);
 
@@ -8,12 +8,12 @@ export function load<T>(key: string, def: T = null): T{
   }
 }
 
-export function save(key: string, value: any): void{
+export function saveToLocalStorage(key: string, value: any): void{
   localStorage.setItem(key, JSON.stringify(value));
 }
 
-export function remove<T = any>(key: string, def: T = null): T{
-  const value: any = load<T>(key, def);
+export function removeFromLocalStorage<T = any>(key: string, def: T = null): T{
+  const value: any = loadFromLocalStorage<T>(key, def);
   localStorage.removeItem(key);
 
   return value;
