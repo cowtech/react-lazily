60a7cc4b2b4c2282523237cb462854aa3a38ca4c
Shogun <shogun@cowtech.it>
2018-01-19 12:58:23 +0100

    Updated CHANGELOG.md.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 2018-01-19 / 0.3.2
+
+* Rebuild JS version.
+
 ### 2018-01-19 / 0.3.1
 
 * Rename confusing methods.
--- lib/utils/local-storage.d.ts
+++ lib/utils/local-storage.d.ts
@@ -1,3 +1,3 @@
-export declare function load<T>(key: string, def?: T): T;
-export declare function save(key: string, value: any): void;
-export declare function remove<T = any>(key: string, def?: T): T;
+export declare function loadFromLocalStorage<T>(key: string, def?: T): T;
+export declare function saveToLocalStorage(key: string, value: any): void;
+export declare function removeFromLocalStorage<T = any>(key: string, def?: T): T;
