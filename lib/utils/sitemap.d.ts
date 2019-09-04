997fe89a6eb1cdc92500cc296ec183520d544f0b
Shogun <shogun@cowtech.it>
2019-09-04 09:28:50 +0200

    chore: Include missing files.

---
+++ lib/utils/sitemap.d.ts
@@ -0,0 +1,19 @@
+import { DetailedHTMLProps, ReactNode, ReactNodeArray } from 'react';
+declare global {
+    namespace JSX {
+        type SitemapElement<T = {}> = DetailedHTMLProps<T & {
+            children?: ReactNode | ReactNodeArray;
+        }, HTMLElement>;
+        interface IntrinsicElements {
+            sitemap: SitemapElement;
+            urlset: SitemapElement<{
+                xmlns?: string;
+            }>;
+            url: SitemapElement;
+            loc: SitemapElement;
+            lastmod: SitemapElement;
+            changefreq: SitemapElement;
+            priority: SitemapElement;
+        }
+    }
+}
